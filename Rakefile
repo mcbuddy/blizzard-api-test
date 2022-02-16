2eef7ef217d2b90894a112e1b7aa3cbb75fdda1e
Budi Sugianto <budi@teachable.com>
2022-02-15 23:31:24 -0500

    Use ENV for the passing arg on rake/rspec

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
 # Rakefile to wrap all the test into one simple command
+#!/usr/bin/env rake
 
 task default:'run:all_tests'
 
@@ -6,7 +7,7 @@ namespace :show do
   desc 'Showing all the task or rake -T'
   task :all do
     print 'Running '
-    sh 'rake -T'
+    system('rake -T')
   end
 end
 
@@ -14,17 +15,17 @@ namespace :run do
   desc 'Run all the tests [default]'
   task :all_tests do
     puts 'Run all the tests'
-    sh 'rspec specs/* --format documentation'
+    system("CLIENT_ID=#{ENV['CLIENT_ID']} CLIENT_SECRET=#{ENV['CLIENT_SECRET']} rspec specs/* --format documentation")
   end
 
   desc 'Run Diablo 3 Community API Test'
   task :diablo3 do
-    sh 'rspec specs/diablo3/* --format documentation'
+    system("CLIENT_ID=#{ENV['CLIENT_ID']} CLIENT_SECRET=#{ENV['CLIENT_SECRET']} rspec specs/diablo3/* --format documentation")
   end
 
   desc 'Run HearthStone Game Data API Test'
   task :hearthstone do
-    sh 'rspec specs/hearthstone/* --format documentation'
+    system("CLIENT_ID=#{ENV['CLIENT_ID']} CLIENT_SECRET=#{ENV['CLIENT_SECRET']} rspec specs/hearthstone/* --format documentation")
   end
 
 end
--- spec_helper.rb
+++ spec_helper.rb
@@ -6,18 +6,18 @@ require 'json'
 require 'yaml'
 require 'pry'
 
-# laod the enviroment variable into memory and assign it to constant ENV
-config_path = File.expand_path("../config/environment.yml", __FILE__)
-ENV = YAML::load_file(config_path)
-
 # module wrapper for the Api Client for cleaner code and DRY
 module ApiTestClient
   include Faraday
 
+  # laod the enviroment variable into memory and assign it to constant ENV
+  config_path = File.expand_path("../config/environment.yml", __FILE__)
+  @environment = YAML::load_file(config_path)
+
   # the function to get access token every each test executed
   # sample call: curl -u $client_id:$client_secret -d grant_type=client_credentials https://us.battle.net/oauth/token
   def get_access_token
-    conn = Faraday::Connection.new ENV["LOGIN_URI"]
+    conn = Faraday::Connection.new(@environment["LOGIN_URI"])
     conn.headers['Authorization']
     conn.params["grant_type"] = "client_credentials"
     conn.set_basic_auth(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
@@ -30,7 +30,7 @@ module ApiTestClient
   # get request wrapper
   # curl https://us.api.blizzard.com/d3/data/act/1?locale=en_US&access_token=USbc9COeeWlzMPinXOQB2T5gyGNOq8v9QF
   def get_request(uri, token)
-    conn = Faraday::Connection.new ENV["BASE_URI"]
+    conn = Faraday::Connection.new(@environment["BASE_URI"])
     conn.params["locale"] = "en_US"
     conn.params["access_token"] = token
     resp = conn.get uri
@@ -41,7 +41,7 @@ module ApiTestClient
 
   # post request wrapper
   def post_request(uri, token)
-    conn = Faraday::Connection.new ENV["BASE_URI"]
+    conn = Faraday::Connection.new(@environment["BASE_URI"])
     conn.params["locale"] = "en_US"
     conn.params["access_token"] = token
     resp = conn.post uri
